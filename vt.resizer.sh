3dcf922047ef6217f86616a2f057057f8bc143d0
Mattia Caneva <canvacanva@gmail.com>
2025-11-17 17:26:13 +0100

    Update vt.resizer.sh

--- vt.resizer.sh
+++ vt.resizer.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# sudo apt install imagemagick
+# prerequisito: sudo apt install imagemagick
+
+echo "converto le immagini"
+cd /var/.../productimages/
+mogrify -verbose -format jpg *.png
+
 echo "converto le immagini ad 800x qualità 100"
-cd /var/www/html/prodotto/storage/productimages/
+cd /var/.../productimages/
 mogrify -verbose -resize 800x800 -quality 100 *.jpg
 
 echo "Concludo - esco"
+exit
