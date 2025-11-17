228fbe38a14fb4dd8df74d54b5a559b7bf4687cb
Mattia Caneva <canvacanva@gmail.com>
2025-11-17 17:23:40 +0100

    Update directory path for image resizing

--- vt.resizer.sh
+++ vt.resizer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # sudo apt install imagemagick
 echo "converto le immagini ad 800x qualità 100"
-cd /var/www/html/vte/storage/productimages/
+cd /var/www/html/prodotto/storage/productimages/
 mogrify -verbose -resize 800x800 -quality 100 *.jpg
 
 echo "Concludo - esco"
